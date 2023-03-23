44fbd352d70e64e5e944e02d578a9805dae33d87
nielae <udunwadaniela@gmail.com>
2023-03-23 23:17:11 +0100

    Providers

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -7,11 +7,11 @@ terraform {
    }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-  
-}
+#provider "aws" {
+  #profile = "default"
+  #region  = "us-east-1"
+
+#}
 
 
 provider "helm" {
